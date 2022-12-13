94d3643b4d04281ce22d9279a0ac329ee5a4c06e
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-12-13 05:21:55 -0600

    working on code; still trying save data to db.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,31 @@
-const path = require('path');
-const router = require('express').Router();
+const path = require("path");
+const router = require("express").Router();
+const db = require("../db/db.json");
+const fs = require("fs");
 
-router.get('/api/notes', (req,res) => {
-    res.sendFile(path.join(__dirname,'../public/index.html'))
+router.get("/api/notes", (req, res) => {
+  console.log(db);
+  res.send(db);
 });
+
+router.post("/api/notes", (req, res) => {
+  console.log("req.body: ", req.body);
+  db.push(req.body);
+  console.log("db: ", db);
+  // res.send(db);
+  //   let dbFile = JSON.parse(db);
+  //   console.log(dbFile);
+  //   let newDB = dbFile.push(req);
+  //   console.log(req);
+  //   console.log(newDB);
+  //   res.send(dbFile);
+  fs.writeFile("../db/db.json", JSON.stringify(db), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  console.log("post writefile db: ", db);
+  //   res.send(db);
+});
+
+module.exports = router;

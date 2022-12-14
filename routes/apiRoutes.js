053cfc5f26075f49d36a8d6dae8f889c2f8bb135
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-12-13 22:39:07 -0600

    added random id for each new note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,16 @@ const path = require("path");
 const router = require("express").Router();
 const db = require("../db/db.json");
 const fs = require("fs");
+const uuid = require("uuid");
 
 router.get("/api/notes", (req, res) => {
   res.send(db);
 });
 
 router.post("/api/notes", (req, res) => {
-  db.push(req.body); // pushing new save data into db.json
+  const { title, text } = req.body; //destructure req.body; create title and text variables
+  const newNote = { title, text, id: uuid() };
+  db.push(newNote); // pushing new save data into db.json
   let dbString = JSON.stringify(db); // changing new db to a string
   // overwriting db with new db
   fs.writeFile("./db/db.json", dbString, (err) => {
